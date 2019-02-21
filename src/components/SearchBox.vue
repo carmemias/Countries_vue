d1c131b064bc8e5228a884c7904fc88e0b8ce5fd
carmemias <info@carmemias.com>
2019-02-21 16:37:03 +0000

    fixed search box

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,13 +1,15 @@
 <template lang="html">
   <div>
   <input type="text" v-model="searchInput" v-on:input="search" />
-    <div v-if="filteredCountries.length > 0">
-      <p v-for="country in filteredCountries">{{country.name}}</p>
+    <div v-if="filteredCountries.length > 0" v-for="country in filteredCountries">
+      <country-details :country='country'></country-details>
     </div>
   </div>
 </template>
 
 <script>
+import CountryDetails from './CountryDetails.vue';
+
 export default {
   data(){
     return {
@@ -17,9 +19,12 @@ export default {
   },
   name: 'search-box',
   props: ['countries'],
+	components: {
+		"country-details": CountryDetails
+	},
   methods: {
     search(){
-      const filteredCountries = this.countries.filter(country => country.name.includes(this.searchInput)?)
+      const filteredCountries = this.countries.filter(country => country.name.toLowerCase().includes(this.searchInput.toLowerCase()));
       this.filteredCountries = filteredCountries
     }
   }
