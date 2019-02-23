d537458419dc51a757591ef4d202cda76e839c20
carmemias <info@carmemias.com>
2019-02-23 06:32:25 +0000

    Improved event bus emit

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,6 +1,6 @@
 <template lang="html">
 <div class="">
-  <select v-on:change="handleChange">
+  <select v-on:change="handleChange" v-model="selectedCountryIndex">
     <option disable value=''>Select a country...</option>
     <option v-for="(country, index) in countries" :key="index" :value="index" >{{country.name}}</option>
   </select>
@@ -12,10 +12,15 @@ import {eventBus} from '../main.js'
 
 export default {
   name: 'countries-list',
+	data(){
+		return {
+			'selectedCountryIndex':''
+		}
+	},
   props: ['countries'],
   methods: {
     handleChange(){
-      eventBus.$emit('clicked-country', this.countries[event.target.value])
+      eventBus.$emit('clicked-country', this.countries[this.selectedCountryIndex])
     }
   }
 }
